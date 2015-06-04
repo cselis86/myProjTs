f1e52e07e548cd680828967cef64d0d1a6c5f091
Elis Hazisllari <cs_elis@live.com>
2015-06-04 12:09:07 +0300

    A* incomlete

---
+++ src/com/elis/salesmen/Rout.java
@@ -0,0 +1,12 @@
+package com.elis.salesmen;
+
+import java.util.ArrayList;
+
+/**
+ * Created by elis on 26/5/2015.
+ */
+public class Rout{
+    public ArrayList<city> cityRout;
+    public int cityRoutLength ;
+
+}
